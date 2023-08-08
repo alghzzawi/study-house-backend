976ecd7c03abbe40a86b2740e31db3f29568f938
alghzzawi <mohammadalghazzawi9@gmail.com>
2023-08-08 10:40:36 +0300

    feat : add json file for deployment

--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = tuple(env.list("ALLOWED_HOSTS"))
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
